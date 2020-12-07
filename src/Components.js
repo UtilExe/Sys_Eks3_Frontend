6f0daa985b0e3ce84c0832f7f361b5f12f453c6c
UtilExe <laymasterlolz@gmail.com>
2020-12-07 12:26:18 +0100

    Implement edit on frontend, including Modal handling!

--- src/Components.js
+++ src/Components.js
@@ -7,7 +7,7 @@ import jwt_decode from "jwt-decode";
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
 import Pagination from 'react-paginate';
-
+import Modal from 'react-modal';
 
 export function Home() {
     return (
@@ -18,50 +18,50 @@ export function Home() {
             </div>
             <div>
                 <h5>How to use our API?</h5>
-                    <ul>
+                <ul>
                     <li>For all visitors of the website:</li>
+                    <ul>
+                        <li>The "Home" tab:</li>
                         <ul>
-                            <li>The "Home" tab:</li>
-                            <ul>
-                                <li>This page!</li>
-                            </ul>
-                            <li>The "Address Info" tab:</li>
-                            <ul>
-                                <li>Enter an address and find the nearest postal box as well as a weather report from the city you entered.</li>
-                            </ul>
-                            <li>The "Login" tab:</li>
-                            <ul>
-                                <li>Login and gain access to more content!</li>
-                            </ul>
+                            <li>This page!</li>
                         </ul>
-
-                        <br></br>
-
-                        <li>If you're logged in (more content):</li>
+                        <li>The "Address Info" tab:</li>
                         <ul>
-                            <li>As user:</li>
-                            <ul>
-                                <li>The "Movies" tab:</li>
-                                <ul>
-                                    <li>Enter a movie title, and get a corresponding review summary and a link to the full review from the New York Times.</li>
-                                </ul>
-                            </ul>
-                            <li>As admin:</li>
-                            <ul>
-                                <li>The "Digital Ocean Info" tab:</li>
-                                <ul>
-                                    <li>Lookup information about the Droplets that the hoster has.</li>
-                                </ul>
-                            </ul>
+                            <li>Enter an address and find the nearest postal box as well as a weather report from the city you entered.</li>
                         </ul>
-
+                        <li>The "Login" tab:</li>
+                        <ul>
+                            <li>Login and gain access to more content!</li>
+                        </ul>
                     </ul>
+
+                    <br></br>
+
+                    <li>If you're logged in (more content):</li>
+                    <ul>
+                        <li>As user:</li>
+                        <ul>
+                            <li>The "Movies" tab:</li>
+                            <ul>
+                                <li>Enter a movie title, and get a corresponding review summary and a link to the full review from the New York Times.</li>
+                            </ul>
+                        </ul>
+                        <li>As admin:</li>
+                        <ul>
+                            <li>The "Digital Ocean Info" tab:</li>
+                            <ul>
+                                <li>Lookup information about the Droplets that the hoster has.</li>
+                            </ul>
+                        </ul>
+                    </ul>
+
+                </ul>
             </div>
         </div>
     );
 }
 
-export function SavedSongs({savedSongs}) {
+export function SavedSongs({ savedSongs }) {
 
     const { song, artist, album } = savedSongs;
     const [songs, setSongs] = useState([]);
@@ -71,76 +71,157 @@ export function SavedSongs({savedSongs}) {
     function handleSubmit(event) {
         event.preventDefault();
         apiFacade.getAllSongs()
-        .then(array => {
-            setSongs(array)
-        })
-        .catch(err => {
-            Promise.resolve(err.fullError).then(function(value) {
-                setError(value.message);
-              })
-          })
+            .then(array => {
+                setSongs(array)
+            })
+            .catch(err => {
+                Promise.resolve(err.fullError).then(function (value) {
+                    setError(value.message);
+                })
+            })
     }
 
     let displaySongs = songs.map((songA) => (
         <ul key={songA.song}>
-        <li> Song: {songA.song} </li>
-        <li> Artist: {songA.artist} </li>
-        <li> Album: {songA.album} </li>
-       </ul>
+            <li> Song: {songA.song} </li>
+            <li> Artist: {songA.artist} </li>
+            <li> Album: {songA.album} </li>
+        </ul>
     ))
 
-        return (
-           <div>
+    return (
+        <div>
             <h2> All Songs</h2>
-             <button className="btn btn-black btnBorder" onClick={handleSubmit}>Get Songs</button>
-             <br/>
-             <br/>
-             {displaySongs}
-             {error}
+            <button className="btn btn-black btnBorder" onClick={handleSubmit}>Get Songs</button>
+            <br />
+            <br />
+            {displaySongs}
+            {error}
         </div>
     )
 }
 
-export function AdminPage(){
-    
+export function AdminPage() {
+
     const [users, setUsers] = useState([]);
 
     function handleSubmit(event) {
         event.preventDefault();
         apiFacade.getAllUsers()
-        .then(array => {
-           setUsers(array)
-        })
+            .then(array => {
+                setUsers(array)
+            })
     }
 
     function handleDelete(username) {
         apiFacade.deleteUser(username)
-        .then(updateUsers);
+            .then(updateUsers);
     }
 
     function updateUsers() {
         apiFacade.getAllUsers()
-        .then(array => {
-           setUsers(array)
-        })
+            .then(array => {
+                setUsers(array)
+            })
     }
 
     let displayUsers = users.map((user) => (
-        <li key={user.username}>Username: {user.username} <br/>
-        <button className="btn btn-black btnBorder" onClick={() => handleDelete(user.username)}>Delete</button></li>
+        <li key={user.username}>Username: {user.username} <br />
+            <button className="btn btn-black btnBorder" onClick={() => handleDelete(user.username)}>Delete</button>
+            <button className="btn btn-black btnBorder" onClick={() => openModal(user.username)}>Edit
+      </button>
+        </li>
     ))
-    
+
+    Modal.setAppElement('#root')
+    const customStyles = {
+        content: {
+            top: '50%',
+            left: '50%',
+            right: 'auto',
+            bottom: 'auto',
+            marginRight: '-50%',
+            transform: 'translate(-50%, -50%)',
+            background: "#1b103a"
+        }
+    };
+
+    const [username, setName] = useState('');
+    const [editedPassword, setEditedPassword] = useState('');
+    const [editMsg, setEditMsg] = useState("");
+    const pStyle = { color: '#19d21f' };
+    const editStyleColor = <p style={ pStyle }>{editMsg}</p>
+
+    function openModal(username) {
+        setIsOpen(true);
+        setName(username);
+    }
+
+    var subtitle;
+    const [modalIsOpen, setIsOpen] = useState(false);
+
+    function afterOpenModal() {
+        // references are now sync'd and can be accessed.
+        subtitle.style.color = '#FFFFFF';
+    }
+
+    function closeModal(username) {
+        setIsOpen(false);
+    }
+
+    function editUserSubmit(event) {
+        event.preventDefault();
+        apiFacade.editUser(username, editedPassword)
+        .then(res => {
+            closeModal();
+            setEditMsg("The password has been updated!");
+          })
+    }
+
+    function handleEditChange(event) {
+        setEditedPassword(event.target.value);
+    };
+
+    function modalShow() {
+        return (
+            <div>
+                <Modal
+                    isOpen={modalIsOpen}
+                    onAfterOpen={afterOpenModal}
+                    onRequestClose={closeModal}
+                    style={customStyles}
+                    contentLabel="Example Modal">
+
+                    <h2 ref={_subtitle => (subtitle = _subtitle)}>Edit {username}</h2>
+                    <form onChange={handleEditChange}>
+                        <div>
+                            <p>Edit password:</p>
+                            <input placeholder="Edit password.." id="editPassword" type="password"/>
+                        </div>
+                        <div>
+                            <button onClick={editUserSubmit} className="btn btn-black btnBorder">Submit</button>
+                            <button onClick={closeModal} className="btn btn-black btnBorder btnBorderTwo">Close</button>
+                        </div>
+                    </form>
+                </Modal>
+            </div>
+        )
+    }
+
     return (
         <div>
             <h2> All Users</h2>
-             <button className="btn btn-black btnBorder" onClick={handleSubmit}>Get Users</button>
-             <p>{displayUsers}</p>
-             <Pagination count={10} />
+            <button className="btn btn-black btnBorder" onClick={handleSubmit}>Get Users</button>
+            <p>{displayUsers}</p>
+            <p >{editStyleColor}</p>
+            <Pagination count={10} />
+            {modalShow()}
+
         </div>
     )
 }
 
-export function BookmarkSong({trackName, artistName, releaseDate, collectionName, bookmark}) {
+export function BookmarkSong({ trackName, artistName, releaseDate, collectionName, bookmark }) {
     const song = trackName;
     const artist = artistName;
     const album = collectionName;
@@ -157,7 +238,7 @@ export function BookmarkSong({trackName, artistName, releaseDate, collectionName
     )
 }
 
-export function SongLookup({bookmark}) {
+export function SongLookup({ bookmark }) {
 
     const init = { song: "", artist: "" };
     const [search, setSearch] = useState(init);
@@ -174,7 +255,7 @@ export function SongLookup({bookmark}) {
             <p><b>Country:</b> {song.country}</p>
             <p><b>Currency:</b> {song.currency}</p>
             <p><b>Collection Name:</b> {song.collectionName}</p>
-            <hr/>
+            <hr />
             <BookmarkSong bookmark={bookmark} trackName={song.trackName} artistName={song.artistName} releaseDate={song.releaseDate} collectionName={song.collectionName} />
         </div>
     ))
@@ -184,8 +265,8 @@ export function SongLookup({bookmark}) {
             <p><b>Similar artist:</b> {song.Name}</p>
             <p><b>Teaser:</b> {song.wTeaser}</p>
             <p><b>Wikipedia URL:</b> {song.wUrl}</p>
-           
-            <hr/>
+
+            <hr />
         </div>
     ))
 
@@ -194,21 +275,21 @@ export function SongLookup({bookmark}) {
     function handleSubmit(event) {
         event.preventDefault();
         apiFacade.getSongInformation(search)
-        .then(data => {
-            setLyrics(data.lyrics.lyrics);
-            setItunes(data.itunes.results);
-            setSimilar(data.similar.Similar.Results);
-        })
+            .then(data => {
+                setLyrics(data.lyrics.lyrics);
+                setItunes(data.itunes.results);
+                setSimilar(data.similar.Similar.Results);
+            })
     }
 
     function handleChange(event) {
-        setSearch({...search, [event.target.id]: event.target.value});
+        setSearch({ ...search, [event.target.id]: event.target.value });
     };
 
     return (
         <div>
             <h2>Song Lookup</h2>
-            <hr/>
+            <hr />
             <div className="wrapper">
                 <form onChange={handleChange}>
                     <div className="row">
@@ -226,8 +307,8 @@ export function SongLookup({bookmark}) {
                     </div>
                 </form>
             </div>
-                
-            <hr/>
+
+            <hr />
             <h3>Information received goes here...</h3>
 
             <Tabs>
@@ -273,27 +354,27 @@ export function Login({ login }) {
         <div>
             <h2>Login here</h2>
             <form onChange={onChange}>
-                <input placeholder="Username" id="username" /> 
-                <br/>
+                <input placeholder="Username" id="username" />
+                <br />
                 <input placeholder="Password" id="password" />
-                <br/>
+                <br />
                 <button onClick={performLogin} type="button" className="btn btn-black btnBorder">Login</button>
             </form>
-            
-            <br/>
-            <hr/>
+
+            <br />
+            <hr />
             <h2>Don't have an account?</h2>
             <Link to={"/sign-up"}>
                 <button type="button" className="btn btn-black btnBorder">Sign Up</button>
             </Link>
-            <hr/>
+            <hr />
         </div>
     )
 }
 
-export function Signup({signup}) {
+export function Signup({ signup }) {
 
-    const init = { username: "", password: "", passwordCheck: ""};
+    const init = { username: "", password: "", passwordCheck: "" };
     const [signUpAcc, setSignUp] = useState(init);
 
     const performSignup = (evt) => {
@@ -308,12 +389,12 @@ export function Signup({signup}) {
         <div>
             <h2>Sign up</h2>
             <form onChange={onChange}>
-                <input placeholder="Username" id="username" /> 
-                <br/>
+                <input placeholder="Username" id="username" />
+                <br />
                 <input placeholder="Password" id="password" />
-                <br/>
+                <br />
                 <input placeholder="Password Checked" id="passwordCheck" />
-                <br/>
+                <br />
                 <button onClick={performSignup} type="button" className="btn btn-black btnBorder">Signup</button>
             </form>
         </div>
@@ -321,10 +402,10 @@ export function Signup({signup}) {
 
 }
 
-export function LoggedIn({username}) {
+export function LoggedIn({ username }) {
 
-const  token = apiFacade.getToken();
-const  decoded = jwt_decode(token); // jwt_decode is an external library
+    const token = apiFacade.getToken();
+    const decoded = jwt_decode(token); // jwt_decode is an external library
     return (
         <div>
             <h2>You are now logged in!</h2>
